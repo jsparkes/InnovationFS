488a03e0a8c75dfaadcaff615d8e3ba991119acc
Jeff Sparkes <jsparkes@gmail.com>
2022-06-06 13:27:26 +0000

    Initial project from template fabulous-xf-vswin.

---
+++ InnovationFS.Android/MainActivity.fs
@@ -0,0 +1,44 @@
+﻿namespace InnovationFS.Android
+
+open System
+
+open Android.App
+open Android.Content
+open Android.Content.PM
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Android.OS
+
+open Fabulous.XamarinForms
+open InnovationFS
+open Xamarin.Forms.Platform.Android
+
+[<Activity(Label = "InnovationFS.Android",
+           Icon = "@drawable/icon",
+           Theme = "@style/MainTheme",
+           MainLauncher = true,
+           ConfigurationChanges = (ConfigChanges.ScreenSize
+                                   ||| ConfigChanges.Orientation))>]
+type MainActivity() =
+    inherit FormsAppCompatActivity()
+
+    do InnovationFS.Android.Resource.UpdateIdValues()
+
+    override this.OnCreate(bundle: Bundle) =
+        FormsAppCompatActivity.TabLayoutResource <- InnovationFS.Android.Resource.Layout.Tabbar
+        FormsAppCompatActivity.ToolbarResource <- InnovationFS.Android.Resource.Layout.Toolbar
+
+        base.OnCreate(bundle)
+        Xamarin.Essentials.Platform.Init(this, bundle)
+        Xamarin.Forms.Forms.Init(this, bundle)
+        this.LoadApplication(Program.startApplication App.program)
+
+    override this.OnRequestPermissionsResult
+        (
+            requestCode: int,
+            permissions: string [],
+            [<GeneratedEnum>] grantResults: Android.Content.PM.Permission []
+        ) =
+        Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults)
+        base.OnRequestPermissionsResult(requestCode, permissions, grantResults)
