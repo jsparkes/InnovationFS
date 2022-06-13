0860f2de012ca76bdb182f50e86ae0ffccacb287
Jeff Sparkes <jsparkes@gmail.com>
2022-06-13 11:25:23 +0000

    Start of image handling code.

---
+++ InnovationFS/Images.fs
@@ -0,0 +1,13 @@
+﻿namespace InnovationFS
+
+module Images =
+
+open Xamarin.Forms
+
+let private loadImage(filename: string) =
+    // TODO add error handling here
+    ImageSource.FromFile(filename)
+
+let Images =
+    System.IO.Directory.GetFiles("c:\\tmp", "*.jpg")
+    |> Array.map loadImage
