a9614785db831f7aa55919521adfa874779b71a8
Jeff Sparkes <jsparkes@gmail.com>
2022-06-14 02:53:13 -0400

    Initial Avalonia.FuncUI skeleton, plus first change adding icons.

---
+++ InnovationFS.UI/Images.fs
@@ -0,0 +1,27 @@
+﻿module Images
+
+open Elmish
+open Avalonia.FuncUI
+open Avalonia.FuncUI.Types
+open System.Diagnostics
+open System.Runtime.InteropServices
+open Avalonia.Controls
+open Avalonia.Layout
+open Avalonia.FuncUI.DSL
+open Avalonia.Media.Imaging
+
+
+let private loadImage(filename: string) : string * IView<Image> =
+    // TODO add error handling here
+    use bm = new Bitmap(filename)
+    (System.IO.Path.GetFileName filename,
+     Image.create [
+         Image.source bm
+     ])
+
+let Images =
+    System.IO.Directory.GetFiles("images", "*.jpg")
+    |> Array.map loadImage
+    |> Map.ofArray
+
+
