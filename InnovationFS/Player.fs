a0f1f17de70c7e2dc3a9547c830859c4a77af791
Jeff Sparkes <jsparkes@gmail.com>
2022-06-16 01:01:00 -0400

    Start of per-player code.

---
+++ InnovationFS/Player.fs
@@ -0,0 +1,23 @@
+﻿module InnovaationFS.Player
+
+open InnovationFS.Cards
+
+// Per player tableau
+// Per color stacks of cards, and the score and achievement files
+type Tableau() =
+    member val stacks = Map.empty<CardColor, Pile>
+
+
+type Hand() =
+    member val cards = Set.empty<Card> with get, set
+    member x.Add(card: Card) : unit = x.cards <- Set.add card x.cards
+    member x.Remove(card: Card) : unit = x.cards <- Set.remove card x.cards
+
+
+type Player(name: string) =
+    member val hand = new Hand() with get, set
+    member val Name = name with get
+
+    
+
+
